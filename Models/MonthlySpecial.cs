de779f7b01f57f70ec242b1c17c53580989dfd9e
Lucas Barreto <lucas.barreto@hbsis.com.br>
2020-10-19 00:12:25 -0300

    Capitulo 5 completo

---
+++ Models/MonthlySpecial.cs
@@ -0,0 +1,10 @@
+﻿namespace curso_explore_california.Models
+{
+    public class MonthlySpecial
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ ViewComponents/MonthlySpecialsViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using curso_explore_california.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace curso_explore_california.ViewComponents
+{
+    [ViewComponent]
+    public class MonthlySpecialsViewComponent : ViewComponent
+    {
+        private readonly BlogDataContext db;
+
+        public MonthlySpecialsViewComponent(BlogDataContext db)
+        {
+            this.db = db;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var specials = db.MonthlySpecials.ToArray();
+            return View(specials);
+        }
+
+    }
+}
